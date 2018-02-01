1891f2ba03a8dc1b7e7acca8de31d8be81c6f121
keef-ui <keef-v@hotmail.co.uk>
2018-02-01 14:23:06 +0000

    dial-chart --- in progress

--- src/charts/DialChart.jsx
+++ src/charts/DialChart.jsx
@@ -4,13 +4,98 @@ import PropTypes from 'prop-types';
 import * as d3 from "d3";
 
 // Based on http://bl.ocks.org/msqr/3202712
-
-
+// another good example https://swizec.com/blog/how-to-make-a-piechart-using-react-and-d3/swizec/6785
 
 
 
 
 export class DialChart extends React.Component {   
+constructor(props) {
+    super();
+
+    this.pie = d3.pie().value((d) => d.value);
+    this.colors = d3.scaleOrdinal(d3.schemeCategory10);
+        this.range = props.maxAngle - this.minAngle;
+    this.r = props.size / 2;
+    this.pointerHeadLength = Math.round(this.r * props.pointerHeadLengthPercent);
+
+    // a linear scale that maps domain values to a percent from 0..1
+    this.scale = d3.scale.linear()
+      .range([0,1])
+      .domain([props.minValue, props.maxValue]);
+      
+    this.ticks = this.scale.ticks(props.majorTicks);
+    this.tickData = d3.range(props.majorTicks).map(function() {return 1/props.majorTicks;});
+    
+    this.arc = d3.svg.arc()
+      .innerRadius(this.r - props.ringWidth - props.ringInset)
+      .outerRadius(this.r - props.ringInset)
+      .startAngle(function(d, i) {
+        var ratio = d * i;
+        let deg =props.minAngle + (this.ratio * this.range)
+        return deg => deg * Math.PI / 180;
+      })
+      .endAngle(function(d, i) {
+        var ratio = d * (i+1);
+        let deg =props.minAngle + (this.ratio * this.range)
+        return deg => deg * Math.PI / 180;
+      });
+
+ }
+  deg2rad(deg) {
+    return deg * Math.PI / 180;
+  }
+  
+  newAngle(d) {
+    var ratio = scale(d);
+    var newAngle = this.props.minAngle + (ratio * range);
+    return newAngle;
+  }
+  renderAnother() {
+    var svg = d3.select(container)
+      .append('svg:svg')
+        .attr('class', 'gauge')
+        .attr('width', this.props.clipWidth)
+        .attr('height', this.props.clipHeight);
+    
+    var centerTx = 'translate('+r +','+ r +')';
+    
+    var arcs = svg.append('g')
+        .attr('class', 'arc')
+        .attr('transform', centerTx);
+    
+    arcs.selectAll('path')
+        .data(tickData)
+      .enter().append('path')
+        .attr('fill', function(d, i) {
+          return this.props.arcColorFn(d * i);
+        })
+        .attr('d', arc);
+    
+    var lg = svg.append('g')
+        .attr('class', 'label')
+        .attr('transform', centerTx);
+    lg.selectAll('text')
+        .data(ticks)
+      .enter().append('text')
+        .attr('transform', function(d) {
+          var ratio = scale(d);
+          var newAngle = this.props.minAngle + (ratio * range);
+          return 'rotate(' +newAngle +') translate(0,' +(this.props.labelInset - r) +')';
+        })
+        .text(this.props.labelFormat);
+
+    var lineData = [ [this.props.pointerWidth / 2, 0], 
+            [0, -pointerHeadLength],
+            [-(this.props.pointerWidth / 2), 0],
+            [0, this.props.pointerTailLength],
+            [this.props.pointerWidth / 2, 0] ];
+        return (
+            <g transform={translate}>
+                {pie.map((d, i) => this.arcGenerator(d, i))}
+            </g>
+        )
+    }
 
  render() {
     // For a real world project, use something like
@@ -24,6 +109,8 @@ export class DialChart extends React.Component {
     // Centers the pie chart
     let x = width / 2;
     let y = height / 2;
+
+   
 
     return (
       <svg width= {width} height={height}>
@@ -86,7 +173,20 @@ class Slice extends React.Component {
         innerRadius:PropTypes.number,
         pi:PropTypes.number,
         chartId:PropTypes.string,
-        color:PropTypes.func 
+        color:PropTypes.func,
+        width:PropTypes.number,
+        height:PropTypes.number,
+        radius:PropTypes.number,
+        innerRadius:PropTypes.number,
+        pi:PropTypes.number,
+        chartId:PropTypes.string,
+        color:PropTypes.func,
+        minValue :PropTypes.number,
+        maxValue: PropTypes.number,
+        minAngle: PropTypes.number,
+        maxAngle: PropTypes.number,
+        majorTicks:PropTypes.number,
+        labelInset: PropTypes.number 
              
     }
     DialChart.defaultProps = {
@@ -97,7 +197,20 @@ class Slice extends React.Component {
             innerRadius:50,
             pi:PropTypes.number,
             chartId: 'halfPie_chart',
-            color: d3.schemeCategory10
+            color: d3.schemeCategory10,
+            width: 300,
+            height: 300,
+            radius:100,
+            innerRadius:50,
+            pi:PropTypes.number,
+            chartId: 'halfPie_chart',
+            color: d3.schemeCategory10,
+            minValue : 0,
+            maxValue: 10,
+            minAngle: -90,
+            maxAngle: 90,
+            majorTicks: 5,
+            labelInset: 10
 
 
     }
